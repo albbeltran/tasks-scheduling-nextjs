c15dce4bea356ac40b7b5cef7bd92e7447683629
Alberto Beltran <albertobeltran@protonmail.com>
2025-06-08 14:56:43 -0600

    Implemented AMPQ library to publish tasks to the tasks queue

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -1,23 +1,25 @@
 import { NextResponse } from 'next/server';
-import { revalidatePath } from 'next/cache';
 import prisma from '@/lib/prisma';
+import { revalidatePath } from 'next/cache';
+import startProducer from '@/lib/pub_task.queue';
 
 export async function POST(request: Request) {
   const body = await request.json();
   try {
-    const newTask = await prisma.tasks.create({
-      data: {
-        name: body.name,
-        duration: parseInt(body.duration),
-        arrival_time: new Date(body.arrival_time),
-        status: body.status, 
-        priority_id: body.priority_id
-      },
-    });
+    // const newTask = await prisma.tasks.create({
+    //   data: {
+    //     name: body.name,
+    //     duration: parseInt(body.duration),
+    //     arrival_time: new Date(body.arrival_time),
+    //     status: body.status, 
+    //     priority_id: body.priority_id
+    //   },
+    // });
 
+    startProducer(JSON.stringify(body))
     revalidatePath('/tasks');
 
-    return NextResponse.json(newTask, { status: 201 });
+    return NextResponse.json("newTask", { status: 201 });
   } catch (error) {
     console.error('Error creating task:', error);
     return NextResponse.json({ message: 'Error al crear la tarea', error }, { status: 500 });
